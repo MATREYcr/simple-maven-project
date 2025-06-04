02556731847f62ff6d35149a6c0e97176d571722
MATREYcr <111471979+MATREYcr@users.noreply.github.com>
2025-06-03 19:34:55 -0500

    test subtraction

--- src/test/java/edu/unac/CalculatorTest.java
+++ src/test/java/edu/unac/CalculatorTest.java
@@ -5,13 +5,19 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    @Test
     void testAddition() {
         Calculator calculator = new Calculator();
         assertEquals(5, calculator.add(2, 3));
     }
-
+    @Test
     void testMultiplication() {
         Calculator calculator = new Calculator();
         assertEquals(6, calculator.multiply(2, 3));
+    }
+    @Test
+    void testSubtraction() {
+        Calculator calculator = new Calculator();
+        assertEquals(1, calculator.subtract(2, 3));
     }
 }
