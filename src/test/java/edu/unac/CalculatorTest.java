53df65c210ec63ffc6da04338f1367c09cb5e5f4
MATREYcr <111471979+MATREYcr@users.noreply.github.com>
2025-06-03 19:00:53 -0500

    CaculatorTest

--- src/test/java/edu/unac/CalculatorTest.java
+++ src/test/java/edu/unac/CalculatorTest.java
@@ -5,4 +5,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
+    void testAddition() {
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.add(2, 3));
+    }
+
+    void testMultiplication() {
+        Calculator calculator = new Calculator();
+        assertEquals(6, calculator.multiply(2, 3));
+    }
 }
